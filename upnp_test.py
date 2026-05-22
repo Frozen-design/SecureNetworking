8b0355809fa75eeaa1a07c33db8e39b3e5a8aee6
Frozen-design <daniel.l.melia@gmail.com>
2026-05-22 17:18:57 -0600

    Added up2p examples in a new file. Needs more teasting since I am unsure why different examples are producing different outcomes

---
+++ upnp_test.py
@@ -0,0 +1,42 @@
+import upnpclient
+import socket
+
+def example1():
+    devices = upnpclient.discover()
+
+    d = devices[0]
+    print(devices)
+    print(d)
+    print(d.WANIPConn1.GetStatusInfo())
+
+
+def example2():
+    msg = 'M-SEARCH * HTTP/1.1\r\nHOST:239.255.255.250:1900\r\nST:upnp:rootdevice\r\nMX:2\r\nMAN:"ssdp:discover"\r\n\r\n'.encode("utf-8")
+
+    # Set up UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    s.settimeout(2)
+    s.sendto(msg, ('239.255.255.250', 1900) ) #type:ignore
+
+    try:
+        while True:
+            data, addr = s.recvfrom(65507)
+            print(addr, data.decode())
+    except socket.timeout:
+        pass
+
+    msg = 'M-SEARCH * HTTP/1.1\r\nHOST:239.255.255.250:1900\r\nST:ssdp:all\r\nMX:2\r\nMAN:"ssdp:discover"\r\n\r\n'.encode("utf-8")
+
+    # Set up UDP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    s.settimeout(2)
+    s.sendto(msg, ('239.255.255.250', 1900) ) #type:ignore
+
+    try:
+        while True:
+            data, addr = s.recvfrom(65507)
+            print(addr, data.decode())
+    except socket.timeout:
+        pass
+
+example1()
