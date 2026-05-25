2b0537e540cfa874d269a0afcf7721d8a33ce727
Frozen-design <daniel.l.melia@gmail.com>
2026-05-25 15:00:06 -0600

    Added functionality for upnp to the server side node. Need to work on connectivity and testing if a different computer on a different network can connect to the server.

--- Server.py
+++ Server.py
@@ -2,6 +2,11 @@ import secrets
 
 import trio
 
+# https://pypi.org/project/upnpclient/
+import upnpclient
+
+import socket
+
 from libp2p import (
     new_host,
 )
@@ -22,6 +27,41 @@ class Server:
     def __init__(self) -> None:
         pass
 
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+def get_external_ip():
+    try:
+        gw_addr = upnpclient.discover()[0].WANIPConn1.GetExternalIPAddress()['NewExternalIPAddress']
+    except:
+        return None
+    return gw_addr
+
+def port_forward(external_port:int, internal_port:int, open_close:str, duration = 60):
+    IP = get_local_ip()
+    devices = upnpclient.discover()
+    d = devices[0]
+    try:
+        d.WANIPConn1.AddPortMapping(
+            NewRemoteHost = '0.0.0.0',
+            NewExternalPort=external_port,
+            NewProtocol='TCP',
+            NewInternalPort=internal_port,
+            NewInternalClient=IP,
+            NewEnabled=open_close,
+            NewPortMappingDescription='Client-server testing',
+            NewLeaseDuration=duration
+        )
+    except:
+        print("port mapping already exists")
 
 async def main():
     # Create a key pair for the host
@@ -43,11 +83,16 @@ async def main():
     # Create a security options dictionary mapping protocol ID to transport
     security_options = {NOISE_PROTOCOL_ID: noise_transport}
 
+    #bootstrap_nodes = ["/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"]
+
     # Create a host with the key pair, Noise security, and mplex multiplexer
-    host = new_host(key_pair=key_pair, sec_opt=security_options)
+    host = new_host(key_pair=key_pair, sec_opt=security_options, enable_upnp=True)
 
     # Configure the listening address using the new paradigm
     port = 7900
+    """port_forward(port, port, '0')
+    public_addr = f"/ip4/{get_external_ip()}/tcp/{port}/p2p/{host.get_id().to_base58()}"
+    print("Public addr:", public_addr)"""
     listen_addrs = get_available_interfaces(port)
     optimal_addr = get_optimal_binding_address(port)
 
--- upnp_test.py
+++ upnp_test.py
@@ -15,34 +15,4 @@ def example1():
     # d.WANIPConn1.actions
     print(d.WANIPConn1.actions)
 
-
-def example2():
-    msg = 'M-SEARCH * HTTP/1.1\r\nHOST:239.255.255.250:1900\r\nST:upnp:rootdevice\r\nMX:2\r\nMAN:"ssdp:discover"\r\n\r\n'.encode("utf-8")
-
-    # Set up UDP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    s.settimeout(2)
-    s.sendto(msg, ('239.255.255.250', 1900) ) #type:ignore
-
-    try:
-        while True:
-            data, addr = s.recvfrom(65507)
-            print(addr, data.decode())
-    except socket.timeout:
-        pass
-
-    msg = 'M-SEARCH * HTTP/1.1\r\nHOST:239.255.255.250:1900\r\nST:ssdp:all\r\nMX:2\r\nMAN:"ssdp:discover"\r\n\r\n'.encode("utf-8")
-
-    # Set up UDP socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    s.settimeout(2)
-    s.sendto(msg, ('239.255.255.250', 1900) ) #type:ignore
-
-    try:
-        while True:
-            data, addr = s.recvfrom(65507)
-            print(addr, data.decode())
-    except socket.timeout:
-        pass
-
 example1()
